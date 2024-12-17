e6e5adb195f4166229e633f80be363122537cb8a
tofayeLL <ahmedtofayel132@gmail.com>
2024-12-17 11:57:19 +0600

    finished edit applicant first table design

--- src/components/Dashboard/IdTracking/EditApplicantInfo/EditApplicantInfo.js
+++ src/components/Dashboard/IdTracking/EditApplicantInfo/EditApplicantInfo.js
@@ -18,27 +18,27 @@ const EditApplicantInfo = () => {
                         {/* <!-- Table Header --> */}
                         <thead class="">
                             <tr>
-                                <th class="border border-gray-300 px-6 py-2 text-left text-gray-700 space-y-3" colspan="2">
+                                <th class="border  border-gray-300 px-6 py-2 text-left space-y-4" colspan="2">
 
                                     {/* button 1 */}
-                                    <button className="block px-6 py-1 text-start bg-red-500 text-gray-900 rounded-md hover:bg-[#73dfb7]  transition-colors ">
+                                    <button className="block px-6 py-1 font-normal text-white text-start bg-red-500 rounded-md hover:bg-[#73dfb7]  transition-colors ">
                                         View Full Profile
                                     </button>
                                     {/* button 1 */}
-                                    <button className="block px-6 py-1 text-start bg-red-500 text-gray-900 rounded-md hover:bg-[#73dfb7]  transition-colors ">
-                                        View profile
+                                    <button className="block px-6 py-1 font-normal text-white text-start bg-yellow-500 rounded-md hover:bg-[#73dfb7]  transition-colors ">
+                                        View Full Profile
                                     </button>
                                     {/* button 1 */}
-                                    <button className="block px-6 py-1 text-start bg-red-500 text-gray-900 rounded-md hover:bg-[#73dfb7]  transition-colors ">
-                                        View profile
+                                    <button className="block px-6 py-1 font-normal text-white text-start bg-blue-500 rounded-md hover:bg-[#73dfb7]  transition-colors ">
+                                        View Full Profile
                                     </button>
                                     {/* button 1 */}
-                                    <button className="block px-6 py-1 text-start bg-red-500 text-gray-900 rounded-md hover:bg-[#73dfb7]  transition-colors ">
-                                        View profile
+                                    <button className="block px-6 py-1 font-normal text-white text-start bg-green-500 rounded-md hover:bg-[#73dfb7]  transition-colors ">
+                                        View Full Profile
                                     </button>
                                     {/* button 1 */}
-                                    <button className="block px-6 py-1 text-start bg-red-500 text-gray-900 rounded-md hover:bg-[#73dfb7]  transition-colors ">
-                                        View profile
+                                    <button className="block px-6 py-1 font-normal text-white text-start bg-orange-500 rounded-md hover:bg-[#73dfb7]  transition-colors ">
+                                        View Full Profile
                                     </button>
 
 
@@ -74,45 +74,50 @@ const EditApplicantInfo = () => {
                         </thead>
 
                         {/* <!-- Table Body --> */}
-                        <tbody>
+                        <tbody className='font-medium text-start '>
                             {/* <!-- Row 1 --> */}
                             <tr class="hover:bg-gray-100 "  >
-                                <td class="border border-gray-300 px-4 py-2" >Row 1, Col 1</td>
-                                <td class="border border-gray-300 px-4 py-2" colspan="3">Row 1, Col 2</td>
-                               
+                                <td class="border border-gray-300 px-4 py-2" >Name</td>
+                                <td class="border border-gray-300 px-4 py-2" colspan="3">Tofayel</td>
+
 
                             </tr>
                             {/* <!-- Repeat Rows --> */}
                             <tr class="hover:bg-gray-100">
-                                <td class="border border-gray-300 px-4 py-2">Row 2, Col 1</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 2, Col 2</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 2, Col 3</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 2, Col 4</td>
+                                <td class="border border-gray-300 px-4 py-2">Applicant Id</td>
+                                <td class="border border-gray-300 px-4 py-2">24000000</td>
+
+                                <td class="border border-gray-300 px-4 py-2">Password</td>
+                                <td class="border border-gray-300 px-4 py-2">76796890708</td>
+                            </tr>
+
+                            <tr class="hover:bg-gray-100">
+                                <td class="border border-gray-300 px-4 py-2">Mobile Number</td>
+                                <td class="border border-gray-300 px-4 py-2">01795627288</td>
+
+                                <td class="border border-gray-300 px-4 py-2">Date of birth</td>
+                                <td class="border border-gray-300 px-4 py-2">1998-01-01
+                                    <br></br> 25 years
+                                </td>
+                            </tr>
+
+                            <tr class="hover:bg-gray-100">
+                                <td class="border border-gray-300 px-4 py-2">Job status</td>
+                                <td class="border border-gray-300 px-4 py-2">Private</td>
+
+                                <td class="border border-gray-300 px-4 py-2">BMRC Reg. N.</td>
+                                <td class="border border-gray-300 px-4 py-2">100043</td>
                             </tr>
                             <tr class="hover:bg-gray-100">
-                                <td class="border border-gray-300 px-4 py-2">Row 3, Col 1</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 3, Col 2</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 3, Col 3</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 3, Col 4</td>
+                                <td class="border border-gray-300 px-4 py-2">payment status</td>
+                                <td class="border border-gray-300 px-4 py-2 flex items-center gap-2">Paid  <button className="block px-2 py-1 text-start bg-red-500 text-white font-normal rounded-md hover:bg-[#73dfb7]  transition-colors ">
+                                    update payment 
+                                </button></td>
+
+                                <td class="border border-gray-300 px-4 py-2">Application Date</td>
+                                <td class="border border-gray-300 px-4 py-2">2024-12-12</td>
                             </tr>
-                            <tr class="hover:bg-gray-100">
-                                <td class="border border-gray-300 px-4 py-2">Row 4, Col 1</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 4, Col 2</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 4, Col 3</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 4, Col 4</td>
-                            </tr>
-                            <tr class="hover:bg-gray-100">
-                                <td class="border border-gray-300 px-4 py-2">Row 5, Col 1</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 5, Col 2</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 5, Col 3</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 5, Col 4</td>
-                            </tr>
-                            <tr class="hover:bg-gray-100">
-                                <td class="border border-gray-300 px-4 py-2">Row 6, Col 1</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 6, Col 2</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 6, Col 3</td>
-                                <td class="border border-gray-300 px-4 py-2">Row 6, Col 4</td>
-                            </tr>
+                           
                         </tbody>
                     </table>
                 </div>
