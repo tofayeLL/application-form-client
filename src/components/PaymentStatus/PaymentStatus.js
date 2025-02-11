8ee9635e6f3181a53dac5c3874724928f6b264bd
tofayeLL <ahmedtofayel132@gmail.com>
2025-02-11 12:30:45 +0600

    added post request for bekash payment

--- src/components/PaymentStatus/PaymentStatus.js
+++ src/components/PaymentStatus/PaymentStatus.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
 
 const PaymentStatus = () => {
+
+    const axiosPublic = useAxiosPublic();
+
     function myfun() {
         // let a = document.getElementsByName('chk');
         // let y = document.getElementById('zx');
@@ -25,19 +29,47 @@ const PaymentStatus = () => {
             // a.disabled = true;
         }
     }
+
+
+
+
+    const pay = async () => {
+
+        try {
+            const data = await axiosPublic.post('/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
+
+            console.log(data.data);
+
+        } catch (error) {
+
+            console.log(error.res.data.message);
+
+        }
+
+    }
     return (
-        <div className='w-[58%] mx-auto'>
-            <h4>Payment Status Page is not available</h4>
-            <span>Input 1</span><input type="checkbox" id="1" name="chk" value="input1" onClick={myfun} onChange={e => console.log(e.target.value)} />
-            <span>Input 2</span><input type="checkbox" id="2" name="chk" value="input2" onClick={myfun} onChange={e => console.log(e.target.value)} />
-            <span>Input 3</span><input type="checkbox" id="3" name="chk" value="input2" onClick={myfun} onChange={e => console.log(e.target.value)} />
-            {/* <span>Input 4</span><input type="checkbox" id="4" name="chk" onClick={myfun} />
+        <section>
+            <div className='w-[58%] mx-auto'>
+                <h4>Payment Status Page is not available</h4>
+                <span>Input 1</span><input type="checkbox" id="1" name="chk" value="input1" onClick={myfun} onChange={e => console.log(e.target.value)} />
+                <span>Input 2</span><input type="checkbox" id="2" name="chk" value="input2" onClick={myfun} onChange={e => console.log(e.target.value)} />
+                <span>Input 3</span><input type="checkbox" id="3" name="chk" value="input2" onClick={myfun} onChange={e => console.log(e.target.value)} />
+                {/* <span>Input 4</span><input type="checkbox" id="4" name="chk" onClick={myfun} />
             <span>Input 5</span><input type="checkbox" id="5" name="chk" onClick={myfun} /> */}
-            {/* <span>Input 6</span><input type="checkbox" id="6" name="chk" onClick={myfun} /> */}
+                {/* <span>Input 6</span><input type="checkbox" id="6" name="chk" onClick={myfun} /> */}
 
 
-            
-        </div>
+
+            </div>
+
+
+
+            <div className='my-20'>
+                <button className="px-4 py-2 bg-[#088658] text-white rounded-lg shadow hover:bg-[#025c3b]">
+                    Bkash Pay
+                </button>
+            </div>
+        </section>
     );
 };
 
